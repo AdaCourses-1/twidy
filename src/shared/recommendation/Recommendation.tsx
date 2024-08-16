4f302c0ee6cae44db06d1d513392403ad3edc8c6
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-16 21:06:59 +0600

    added styles for reccomendatios

--- src/shared/recommendation/Recommendation.tsx
+++ src/shared/recommendation/Recommendation.tsx
@@ -3,7 +3,8 @@ const Recommendation = () => {
     {
       imgURL: "src/assets/Photo.png",
       name: "Екатерина Варнава",
-      description: "Описание Екатери́на Влади́мировна Варна́ва — российская КВНщица, телеведущая. Участник и хореограф юмористическог...",
+      description:
+        "Описание Екатери́на Влади́мировна Варна́ва — российская КВНщица, телеведущая. Участник и хореограф юмористическог...",
       price: "10$",
     },
     {
@@ -39,24 +40,33 @@ const Recommendation = () => {
   ];
 
   return (
-    <div className="container mb-28">
-      <div className="ml-7 min-h-28 content-center">
-        <h1 className="text-[#4E3F6F] font-bold text-4xl  ">Рекомендуем</h1>
+    <div className="mb-28">
+      <div className="mt-10 pl-10 mb-4">
+        <h2 className="text-[#4E3F6F] font-bold text-4xl ">Рекомендуем</h2>
       </div>
-      <div className="flex flex-wrap gap-7"> 
+      <div className="flex flex-wrap gap-7">
         {recommendationActer.map((item, index) => (
-          <div key={index} className={`bg-[#FFFFFF] rounded-2xl m-4 relative ${index === 0 ? 'max-w-[571px]' : 'max-w-[241px]'}`}> 
-            <img src={item.imgURL} alt={item.name} className="max-w-full max-h-[263px] rounded-t-2xl" />
+          <div
+            key={index}
+            className={`bg-[#FFFFFF] rounded-2xl m-4 relative ${
+              index === 0 ? "max-w-[571px]" : "max-w-[241px]"
+            }`}
+          >
+            <img
+              src={item.imgURL}
+              alt={item.name}
+              className="max-w-full max-h-[263px] rounded-t-2xl"
+            />
 
             <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-5 top-64">
-              <p className="text-[#FFFFFF] px-4 py-1 font-bold text-base"> {/* Удалены отступы pl-4 pr-4 pt-1 pb-1/2 mb-1 */}
+              <p className="text-[#FFFFFF] px-4 py-1 font-bold text-base">
+                {" "}
+                {/* Удалены отступы pl-4 pr-4 pt-1 pb-1/2 mb-1 */}
                 {item.price}
               </p>
             </div>
             <div className="mt-7 ml-4 max-w-[530px]">
-              <h3 className="text-[#4E3F6F] font-bold text-lg ">
-                {item.name}
-              </h3>
+              <h3 className="text-[#4E3F6F] font-bold text-lg ">{item.name}</h3>
               <span className="text-[#8C8CB6] font-semibold text-base ">
                 {item.description}
               </span>
