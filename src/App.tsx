8b565da540e7637bac1c69d44b9737ed5b3d1d38
Kostenko Oleg <ok609035@gmail.com>
2024-08-16 06:19:17 +0300

    add new section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./shared/header/Header";
-import './App.css';
+import "./App.css";
+import Recommendation from "./shared/recommendation/Recommendation";
+
 
 function App() {
   return (
     <div className="pt-10 bg-[#F2F2FE] min-h-[100vh] max-w-[1440px] mx-auto">
       <Header />
+      <Recommendation/>
       <main></main>
     </div>
   );
---
+++ src/shared/recommendation/Recommendation.tsx
@@ -0,0 +1,113 @@
+import "./Recommendation.css";
+
+
+
+const Recommendation = () => {
+
+  return (
+    <div className="container">
+      <div className="ml-7 min-h-28 content-center">
+        <h1 className="text-[#4E3F6F] font-bold text-4xl  ">Рекомендуем</h1>
+      </div>
+      <div className="flex gap-7">
+        <div className="m-4 column relative">
+          <img className="" src="src/assets/Photo.png" alt="sdfghj" />
+          <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-5 top-64">
+            <p className=" text-[#FFFFFF] pl-4 pr-4 pt-1 pb-1 font-bold text-base">
+              10$
+            </p>
+          </div>
+
+          <div className=" description mt-7 ml-4">
+            <h3 className="text-[#4E3F6F] font-bold text-lg ">
+              Екатерина Варнава
+            </h3>
+            <span className="text-[#8C8CB6] font-semibold text-base ">
+              Описание Екатери́на Влади́мировна Варна́ва — российская КВНщица,
+              телеведущая. Участник и хореограф юмористическог...
+            </span>
+          </div>
+        </div>
+        <div className="m-4 column-mini relative">
+          <img src="src/assets/Photo (1).png" alt="wefg" />
+          <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-11 top-64">
+            <p className=" text-[#FFFFFF] pl-4 pr-4 pt-1 pb-1/2 mb-1 font-bold text-base">
+              10$
+            </p>
+          </div>
+
+          <div className=" mt-7 ml-4">
+            <h3 className="text-[#4E3F6F] font-bold text-lg ">Айза Анюхина</h3>
+            <span className="text-[#8C8CB6] font-semibold text-base ">
+              Актёр и шоумен. Ведущий программы Вечерний Ур...
+            </span>
+          </div>
+        </div>
+        <div className="m-4 column-mini relative">
+          <img src="src/assets/Photo (2).png" alt="wefg" />
+          <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-11 top-64">
+            <p className=" text-[#FFFFFF] pl-4 pr-4 pt-1 pb-1/2 mb-1 font-bold text-base">
+              10$
+            </p>
+          </div>
+
+          <div className=" mt-7 ml-4">
+            <h3 className="text-[#4E3F6F] font-bold text-lg ">Егор Крид</h3>
+            <span className="text-[#8C8CB6] font-semibold text-base ">
+              Российская бьюти-блогер, бывшая жена Тимати ре...
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="flex mt-10">
+        <div className="m-4 column-mini relative">
+          <img src="src/assets/Photo (3).png" alt="wefg" />
+          <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-11 top-64">
+            <p className=" text-[#FFFFFF] pl-4 pr-4 pt-1 pb-1/2 mb-1 font-bold text-base">
+              10$
+            </p>
+          </div>
+
+          <div className=" mt-7 ml-4">
+            <h3 className="text-[#4E3F6F] font-bold text-lg ">Екатерина Варнава</h3>
+            <span className="text-[#8C8CB6] font-semibold text-base ">
+            Актер и шоумен. Ведущий программы Вечерний Ург...
+            </span>
+          </div>
+        </div>
+        <div className="m-4 column-mini relative">
+          <img src="src/assets/Photo (4).png" alt="wefg" />
+          <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-11 top-64">
+            <p className=" text-[#FFFFFF] pl-4 pr-4 pt-1 pb-1/2 mb-1 font-bold text-base">
+              10$
+            </p>
+          </div>
+
+          <div className=" mt-7 ml-4">
+            <h3 className="text-[#4E3F6F] font-bold text-lg ">Айза Анюхина</h3>
+            <span className="text-[#8C8CB6] font-semibold text-base ">
+            Российская бьюти-блогер, бывшая жена Тимати ре... 
+            </span>
+          </div>
+        </div>
+        <div className="m-4 column-mini relative">
+          <img src="src/assets/Photo (5).png" alt="wefg" />
+          <div className="absolute -mt-60 bg-[#FFA012] rounded-md right-11 top-64">
+            <p className=" text-[#FFFFFF] pl-4 pr-4 pt-1 pb-1/2 mb-1 font-bold text-base">
+              10$
+            </p>
+          </div>
+
+          <div className=" mt-7 ml-4">
+            <h3 className="text-[#4E3F6F] font-bold text-lg ">Айза Анюхина</h3>
+            <span  className=" text-[#8C8CB6] font-semibold text-base ">
+            Актёр и шоумен. Ведущий программы Вечерний Ур...
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recommendation;
