4635e9b9fc7fbefe0ed0523df836fc27c771cdf8
Kostenko Oleg <ok609035@gmail.com>
2024-08-16 20:32:19 +0300

    /

---
+++ src/shared/authorizationDrawer/AuthorizationDrawer.tsx
@@ -0,0 +1,63 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Tabs, TabsList, TabsTrigger } from "@radix-ui/react-tabs";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export function AuthorizationDrawer(props: LayoutProps) {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>{props?.children}</SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <Tabs>
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="sign-in">Sign-in</TabsTrigger>
+            <TabsTrigger value="sign-up">Sign-up</TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-right">
+              Email
+            </Label>
+            <Input
+              id="email"
+              value="Jyldyzakylbekova@gmail.com"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="password" className="text-right">
+              Username
+            </Label>
+            <Input id="password" value="ice-capuchino" className="col-span-3" />
+          </div>
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
