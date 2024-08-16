6a761ac44d9e7adaef9c7be35d4686782986b3a5
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-16 21:03:15 +0600

    added auth drawer

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="pt-10 bg-[#F2F2FE] min-h-[100vh] max-w-[1440px] mx-auto">
       <Header />
-      <main className="pl-12">
+      <main className="pl-12 pr-14">
         <Category />
       </main>
     </div>
