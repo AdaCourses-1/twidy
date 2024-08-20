ae5f3f10c0b0ec25f81d77a1108e07d7b327555c
Namelesss01 <zajloobekovali00@gmail.com>
2024-08-21 02:24:34 +0600

    скрытие при нажатии

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,23 +1,23 @@
 import Header from "@/shared/header/Header";
-import userAvatar from "../../assets/Photo (3).png";
-import { TwitterIcon } from "lucide-react";
-import Instagramm from "../../assets/inst.svg";
+import userAvatar from "../../assets/user.png";
+
+import Instagram from "../../assets/inst.svg";
 import Twitter from "../../assets/twit.svg";
 import Vk from "../../assets/vk.svg";
 import { Separator } from "@radix-ui/react-separator";
+import { Button } from "@/components/ui/button";
+import UserServices from "@/shared/userServices/UserServices";
 const Settings = () => {
   return (
-
     <div className="settings-page w-full">
-
       <Header />
       <div>
         <h3 className="text-[#4E3F6F] font-bold text-4xl pl-16 pt-10 ">
           Дарья Небесная
         </h3>
 
-        <div className="flex pt-8 pl-10">
-          <aside className="p-4 bg-white rounded-[20px] max-w-[400px]">
+        <div className="flex-1 pt-8 pl-10">
+          <aside className="p-4 bg-white rounded-[20px] max-w-[410px]">
             <div className="relative">
               <span className="absolute left-4 top-4 bg-[#FFA012] rounded-lg text-white py-1 p-3 text-base font-bold">
                 @Ahmedka
@@ -25,7 +25,7 @@ const Settings = () => {
               <img
                 width={380}
                 height={380}
-                src={useravatar}
+                src={userAvatar}
                 alt="Дарья Небесная"
                 className="object-cover"
               />
@@ -93,7 +93,6 @@ const Settings = () => {
                   <span className="text-base text-[#8C8CB6] font-bold">
                     время ответа
                   </span>
-
                 </li>
               </ul>
               <Separator
@@ -105,20 +104,38 @@ const Settings = () => {
             <ul className="pt-6 pb-9">
               <li className="flex items-end gap-2 mb-3">
                 <span className="text-4xl text-[#4E3F6F] font-bold">169к</span>
-                <span className="text-base text-[#8C8CB6] font-bold"> подписчиков</span>
+                <span className="text-base text-[#8C8CB6] font-bold">
+                  {" "}
+                  подписчиков
+                </span>
               </li>
               <li className="flex items-end gap-2 mb-3">
                 <span className="text-4xl text-[#4E3F6F] font-bold">4.3</span>
-                <span className="text-base text-[#8C8CB6] font-bold">рейтинг</span>
+                <span className="text-base text-[#8C8CB6] font-bold">
+                  рейтинг
+                </span>
               </li>
               <li className="flex items-end gap-2 mb-3">
                 <span className="text-4xl text-[#4E3F6F] font-bold">3ч.</span>
-                <span className="text-base text-[#8C8CB6] font-bold">время ответа</span>
+                <span className="text-base text-[#8C8CB6] font-bold">
+                  время ответа
+                </span>
               </li>
             </ul>
+          </aside>
+          <div className="flex item-center pt-6">
+            <Button vertical="ghost">Заблокировать</Button>
+            <Separator
+              orientation="vertical"
+              className="w-[3px] h-8 rounded-sm"
+            />
+            <Button vertical="ghost">Пожаловаться</Button>
           </div>
-        </aside>
-        <div></div>
+        </div>
+        <div className="flex-1">
+          <UserServices />
+          <div></div>
+        </div>
       </div>
     </div>
   );
--- src/shared/category/Category.tsx
+++ src/shared/category/Category.tsx
@@ -1,36 +1,100 @@
+import React, { useState, useEffect } from "react";
+
 const Category = () => {
   const items = [
-    { name: "Музыканты", imgURL: "src/assets/Music.svg" },
-    { name: "Актёры", imgURL: "src/assets/acter.svg" },
-    { name: "YouTubers", imgURL: "src/assets/youtuber.svg" },
-    { name: "Блогеры", imgURL: "src/assets/bloger.svg" },
-    { name: "Комики", imgURL: "src/assets/comic.svg" },
-    { name: "Модели", imgURL: "src/assets/model.svg" },
+    {
+      name: "Музыканты",
+      imgURL: "src/assets/Music.svg",
+      title: "Категория “Музыканты”",
+    },
+    {
+      name: "Актёры",
+      imgURL: "src/assets/acter.svg",
+      title: "Категория “Актёры”",
+    },
+    {
+      name: "YouTubers",
+      imgURL: "src/assets/youtuber.svg",
+      title: "Категория “YouTubers”",
+    },
+    {
+      name: "Блогеры",
+      imgURL: "src/assets/bloger.svg",
+      title: "Категория “Блогеры”",
+    },
+    {
+      name: "Комики",
+      imgURL: "src/assets/comic.svg",
+      title: "Категория “Комики”",
+    },
+    {
+      name: "Модели",
+      imgURL: "src/assets/model.svg",
+      title: "Категория “Модели”",
+    },
   ];
+
+  // Получение состояния из localStorage или установка начального значения
+  const [selectedCategory, setSelectedCategory] = useState(() => {
+    const savedCategory = localStorage.getItem("selectedCategory");
+    return savedCategory ? JSON.parse(savedCategory) : null;
+  });
+
+  // Сохранение состояния в localStorage при изменении
+  useEffect(() => {
+    if (selectedCategory) {
+      localStorage.setItem(
+        "selectedCategory",
+        JSON.stringify(selectedCategory)
+      );
+    } else {
+      localStorage.removeItem("selectedCategory");
+    }
+  }, [selectedCategory]);
+
+  const handleCardClick = (item) => {
+    setSelectedCategory(item);
+  };
+
+  const handleBackClick = () => {
+    setSelectedCategory(null);
+  };
 
   return (
     <div className="pl-14">
       <h3 className="text-[#4E3F6F] font-bold text-4xl pl-10 pt-24">
-        Категори
+        {selectedCategory ? selectedCategory.title : "Категори"}
       </h3>
 
-      <div className="flex flex-wrap justify-between gap-7 mb-7">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="flex items-center justify-around gap-20 mt-7 bg-[#Fff] w-[100%] xl:w-[30%] h-[114px] rounded-[20px]"
-          >
-            <span className=" text-lg text-[#4E3F6F] font-bold">
-              {item.name}
-            </span>
-            <img
-              src={item.imgURL}
-              className="w-[119px] h-[84px] rounded-[20px]"
-              alt={item.name}
-            />
-          </div>
-        ))}
-      </div>
+      {selectedCategory ? (
+        <button
+          onClick={handleBackClick}
+          className="text-[#4E3F6F] font-bold text-lg pl-10 pt-4"
+        >
+          Назад
+        </button>
+      ) : null}
+
+      {!selectedCategory && (
+        <div className="flex flex-wrap justify-between gap-7 mb-7">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => handleCardClick(item)}
+              className="flex items-center justify-around gap-20 mt-7 bg-[#Fff] w-[100%] xl:w-[30%] h-[114px] rounded-[20px] cursor-pointer"
+            >
+              <span className="text-lg text-[#4E3F6F] font-bold">
+                {item.name}
+              </span>
+              <img
+                src={item.imgURL}
+                className="w-[119px] h-[84px] rounded-[20px]"
+                alt={item.name}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
