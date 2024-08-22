3cdbd28588dcacbacf7804f79f98a56bf6749836
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-22 20:49:36 +0600

    added types and const

---
+++ src/shared/answer/const.ts
@@ -0,0 +1,29 @@
+import Rectongle from '@/assets/Rectongle.png';
+import { AnswerItem } from './types';
+
+export const ANSWER_ITEMS: AnswerItem[] = [
+  {
+    title: 'Поздравление для Александра',
+    date: '2 часа назад',
+    imgUrl: Rectongle,
+    duration: '1:24',
+  },
+  {
+    title: 'Поздравление для Александра',
+    date: '2 часа назад',
+    imgUrl: Rectongle,
+    duration: '1:24',
+  },
+  {
+    title: 'Поздравление для Александра',
+    date: '2 часа назад',
+    imgUrl: Rectongle,
+    duration: '1:24',
+  },
+  {
+    title: 'Поздравление для Александра',
+    date: '2 часа назад',
+    imgUrl: Rectongle,
+    duration: '1:24',
+  },
+];
