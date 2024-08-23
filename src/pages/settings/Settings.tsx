fc78cd833c1ed651c8bc7e6008cb8bde52429dde
Atabek Kurbanov <yuzeyzer.atabek@gmail.com>
2024-08-23 19:46:18 +0600

    added spaces

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -1,6 +1,8 @@
 import Header from '@/shared/header/Header';
 import HistoryTransaction from '@/shared/history/History';
 
+
+
 const Settings = () => {
   return (
     <div className="settings-page w-full mb-20">
