1806ff47b2a283fed12a9078c2ce8e41072cb231
Namelesss01 <zajloobekovali00@gmail.com>
2024-08-23 19:45:27 +0600

    LAST CATEGORY

--- src/shared/category/Category.tsx
+++ src/shared/category/Category.tsx
@@ -1,49 +1,14 @@
 import React, { useState } from 'react';
+import { CATEGORY_ITEMS } from './const'; // Убедитесь, что путь верен
+import { CategoryItem } from './types'; // Убедитесь, что путь верен
 
 const Category = () => {
-  const items = [
-    {
-      id: 1,
-      name: 'Музыканты',
-      imgURL: 'src/assets/Music.svg',
-      title: 'Категория "Музыканты"',
-    },
-    {
-      id: 2,
-      name: 'Актёры',
-      imgURL: 'src/assets/acter.svg',
-      title: 'Категория "Актёры"',
-    },
-    {
-      id: 3,
-      name: 'YouTubers',
-      imgURL: 'src/assets/youtuber.svg',
-      title: 'Категория "YouTubers"',
-    },
-    {
-      id: 4,
-      name: 'Блогеры',
-      imgURL: 'src/assets/bloger.svg',
-      title: 'Категория "Блогеры"',
-    },
-    {
-      id: 5,
-      name: 'Комики',
-      imgURL: 'src/assets/comic.svg',
-      title: 'Категория "Комики"',
-    },
-    {
-      id: 6,
-      name: 'Модели',
-      imgURL: 'src/assets/model.svg',
-      title: 'Категория "Модели"',
-    },
-  ];
+  const [selectedCategory, setSelectedCategory] = useState<CategoryItem | null>(
+    null
+  );
 
-  const [selectedCategory, setSelectedCategory] = useState(null);
-
-  const handleCardClick = (item) => {
-    setSelectedCategory(item);
+  const handleCardClick = (category: CategoryItem) => {
+    setSelectedCategory(category);
   };
 
   const handleBackClick = () => {
@@ -53,7 +18,7 @@ const Category = () => {
   return (
     <div className="pl-14">
       <h3 className="text-[#4E3F6F] font-bold text-4xl pl-10 pt-24">
-        {selectedCategory ? selectedCategory.title : 'Категории'}
+        {selectedCategory?.title || 'Категории'}
       </h3>
 
       {selectedCategory && (
@@ -67,19 +32,19 @@ const Category = () => {
 
       {!selectedCategory && (
         <div className="flex flex-wrap justify-between gap-7 mb-7">
-          {items.map((item) => (
+          {CATEGORY_ITEMS.map((category) => (
             <div
-              key={item.id}
-              onClick={() => handleCardClick(item)}
+              key={category.id}
+              onClick={() => handleCardClick(category)}
               className="flex items-center justify-around gap-20 mt-7 bg-[#Fff] w-[100%] xl:w-[30%] h-[114px] rounded-[20px] cursor-pointer"
             >
               <span className="text-lg text-[#4E3F6F] font-bold">
-                {item.name}
+                {category.name}
               </span>
               <img
-                src={item.imgURL}
+                src={category.imgURL}
                 className="w-[119px] h-[84px] rounded-[20px]"
-                alt={item.name}
+                alt={category.name}
               />
             </div>
           ))}
---
+++ src/shared/category/const.ts
@@ -0,0 +1,45 @@
+import Music from '@/assets/Music.svg';
+import Acter from '@/assets/acter.svg';
+import YouTuber from '@/assets/youtuber.svg';
+import Bloger from '@/assets/bloger.svg';
+import Comic from '@/assets/comic.svg';
+import { CategoryItem } from './types';
+
+export const CATEGORY_ITEMS: CategoryItem[] = [
+  {
+    id: 0,
+    name: 'Музыканты',
+    imgURL: Music,
+    title: 'Категория "Музыканты"',
+  },
+  {
+    id: 1,
+    name: 'Актёры',
+    imgURL: Acter,
+    title: 'Категория "Актёры"',
+  },
+  {
+    id: 2,
+    name: 'YouTubers',
+    imgURL: YouTuber,
+    title: 'Категория "YouTubers"',
+  },
+  {
+    id: 3,
+    name: 'Блогеры',
+    imgURL: Bloger,
+    title: 'Категория "Блогеры"',
+  },
+  {
+    id: 4,
+    name: 'Комики',
+    imgURL: Comic,
+    title: 'Категория "Комики"',
+  },
+  {
+    id: 5,
+    name: 'Модели',
+    imgURL: Music,
+    title: 'Категория "Модели"',
+  },
+];
