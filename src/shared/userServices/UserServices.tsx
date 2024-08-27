f222ae1d68ebe21c9ede695827af99c62a33364a
Narizizu <narizizu@gmail.com>
2024-08-27 20:25:18 +0600

    changed the code

--- src/shared/userServices/UserServices.tsx
+++ src/shared/userServices/UserServices.tsx
@@ -3,12 +3,15 @@ import { Button } from "@/components/ui/button";
 import { SERVICE_ITEMS } from "./const";
 import { ServiceItem } from "./types";
 
+
+
 const UserServices = () => {
   return (
     <div>
       <h3 className="text-[#4E3F6F] font-bold text-4xl pl-10 mb-6">Услуги</h3>
       <div className="ml-2 flex-1 flex gap-5 flex-col">
         {SERVICE_ITEMS.map((service: ServiceItem) => (
+          
           <div key={service.name} className="flex items-center justify-between bg-white p-8 rounded-[20px]">
             <div>
               <h5 className="text-lg font-bold text-[#4E3F6F]">
