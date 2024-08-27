c3962395cf5c38a93e2cddd24f78c1dbd6537214
jyldyzakylbekova <akylbekova@csdisco.com>
2024-08-27 15:37:31 +0600

    deleted spaces

--- src/pages/settings/Settings.tsx
+++ src/pages/settings/Settings.tsx
@@ -2,8 +2,6 @@ import ChannelsFollowers from '@/shared/channelsFollowers/ChannelsFollowers';
 import Header from '@/shared/header/Header';
 import HistoryTransaction from '@/shared/history/History';
 
-
-
 const Settings = () => {
   return (
     <div className="settings-page w-full mb-20">
