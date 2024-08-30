aa88a01f997a1746e62ea083b27cb4292aa48c31
Kostenko Oleg <ok609035@gmail.com>
2024-08-30 19:31:26 +0600

    fix

--- src/pages/messages/Messages.tsx
+++ src/pages/messages/Messages.tsx
@@ -1,13 +1,15 @@
-import GeneralMaterials from "@/shared/generalMaterials/GeneralMaterials";
-import Header from "@/shared/header/Header";
-import VideoSection from "@/shared/video/VideoSection";
+import GeneralMaterials from '@/shared/generalMaterials/GeneralMaterials';
+import Header from '@/shared/header/Header';
+import VideoSection from '@/shared/video/VideoSection';
 
 const Messages = () => {
-  return <div className="settings-page w-full mb-20">
-    <Header/>
-    <GeneralMaterials/>
-    <VideoSection />
-  </div>;
+  return (
+    <div className='w-full mb-20'>
+      <Header />
+      <GeneralMaterials />
+      <VideoSection />
+    </div>
+  );
 };
 
 export default Messages;
