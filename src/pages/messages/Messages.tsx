be30e4475580e7aba71912c25e13298eba0e4b8d
Kostenko Oleg <ok609035@gmail.com>
2024-08-30 19:29:22 +0600

    I didn't add anything new, I just corrected it

--- src/pages/messages/Messages.tsx
+++ src/pages/messages/Messages.tsx
@@ -1,16 +1,13 @@
-import VideoSection from '@/shared/video/VideoSection';
-import Header from '@/shared/header/Header';
-import GeneralMaterials from '@/shared/generalMaterials/GeneralMaterials';
-
+import GeneralMaterials from "@/shared/generalMaterials/GeneralMaterials";
+import Header from "@/shared/header/Header";
+import VideoSection from "@/shared/video/VideoSection";
 
 const Messages = () => {
-  return (
-    <div className="settings-page w-full mb-20">
-      <Header />
-      <GeneralMaterials />
-      <VideoSection />
-    </div>
-  );
+  return <div className="settings-page w-full mb-20">
+    <Header/>
+    <GeneralMaterials/>
+    <VideoSection />
+  </div>;
 };
 
 export default Messages;
