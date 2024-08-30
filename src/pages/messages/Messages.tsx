697875d5c09a1145ab0f814e8d62b05af2a82652
Kostenko Oleg <ok609035@gmail.com>
2024-08-30 20:26:17 +0600

    fix pr

--- src/pages/messages/Messages.tsx
+++ src/pages/messages/Messages.tsx
@@ -1,15 +1,13 @@
-import GeneralMaterials from '@/shared/generalMaterials/GeneralMaterials';
-import Header from '@/shared/header/Header';
-import VideoSection from '@/shared/video/VideoSection';
+import VideoSection from "@/shared/video/VideoSection";
+import GeneralMaterials from "@/shared/generalMaterials/GeneralMaterials";
+import Header from "@/shared/header/Header";
 
 const Messages = () => {
-  return (
-    <div>
-      <Header />
-      <GeneralMaterials />
-      <VideoSection />
-    </div>
-  );
+  return <div>
+    <Header />
+    <GeneralMaterials />
+    <VideoSection />
+  </div>;
 };
 
 export default Messages;
