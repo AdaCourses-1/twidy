9794ef1a05cc353c807b6b74e56d3b7d0d0c2086
Narizizu <narizizu@gmail.com>
2024-08-30 19:17:11 +0600

    created new code

---
+++ src/shared/photo/const.ts
@@ -0,0 +1,16 @@
+import { PhotoItem } from "./types";
+import Photo1 from '@/assets/Photo (1).png';
+import Varnava2 from '@/assets/varnava2.png';
+import Photo3 from '@/assets/Photo (3).png';
+
+export const PHOTO_ITEMS: PhotoItem[] = [
+    { id:1, photoURL: Photo1, },
+    { id:1, photoURL: Photo3, },
+    { id:1, photoURL: Varnava2, },
+    { id:1, photoURL: Photo1, },
+    { id:1, photoURL: Photo3, },
+    { id:1, photoURL: Varnava2, },
+    { id:1, photoURL: Photo1, },
+    { id:1, photoURL: Photo3, },
+    { id:1, photoURL: Varnava2, },
+]
