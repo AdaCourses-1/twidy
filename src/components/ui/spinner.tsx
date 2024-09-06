fd6329d2a40a26b28530d427812ceca4fccca119
jyldyzakylbekova <akylbekova@csdisco.com>
2024-09-06 14:46:20 +0600

    resolvedconf

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -23,5 +23,3 @@ const Spinner = () => {
 };
 
 export default Spinner;
-
-export default Spinner;
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,39 +1,3 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { useContext, useState } from "react";
-import { auth, db } from "../firebase/config";
-import { AuthContext } from "../context/AuthContext";
-import { doc, updateDoc } from "firebase/firestore";
-
-export const useLogin = () => {
-  const [error, setError] = useState<string>('');
-  const [isPending, setIsPending] = useState(false);
-  const { dispatch } = useContext<any>(AuthContext);
-
-  const login = async (email: string, password: string) => {
-    setError('');
-    setIsPending(true);
-
-    try {
-      const response = await signInWithEmailAndPassword(auth, email, password);
-
-      if (!response) {
-        setError("Error during login");
-      }
-
-      const userDoc = doc(db, "users", response.user.uid);
-
-      await updateDoc(userDoc, { online: true });
-
-      dispatch({ type: "LOGIN", payload: response.user });
-    } catch (err: any) {
-      setError(err.message);
-    } finally {
-      setIsPending(false);
-    }
-  };
-
-  return { error, isPending, login };
-};
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useContext, useState } from 'react';
 import { auth, db } from '../firebase/config';
